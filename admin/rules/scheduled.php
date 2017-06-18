fd09536cd0a763d6f764987fe1674851802ac2a8
ChrisBAshton <chrisashtonweb@gmail.com>
2017-06-18 22:24:48 +0100

    hard-coded 3pm schedule date

--- admin/rules/scheduled.php
+++ admin/rules/scheduled.php
@@ -1,5 +1,12 @@
 <?php
 
 function article_health__scheduled($rules, $postID) {
-    return ['@TODO article_health__scheduled'];
+    $errors = [];
+    if (get_post_status($postID) === 'draft' || get_post_status($postID) === 'auto-draft') {
+        $errors[] = "This post needs a schedule date!";
+    }
+    else if ('15:00' !== get_the_date('H:i', $postID)) {
+        $errors[] = "All posts should go live at 15:00 on any given day.";
+    }
+    return $errors;
 }
