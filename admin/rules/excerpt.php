a2355249e39d07f61b832e9d71eb35c7c8fee08e
ChrisBAshton <chrisashtonweb@gmail.com>
2017-06-18 21:29:56 +0100

    defined excerpt rules

--- admin/rules/excerpt.php
+++ admin/rules/excerpt.php
@@ -1,5 +1,19 @@
 <?php
 
 function article_health__excerpt($rules, $postID) {
-    return ['@TODO article_health__excerpt'];
+    $errors = [];
+    if (!has_excerpt($postID)) {
+        $errors[] = "You must specify an excerpt!";
+    }
+    else {
+        $excerpt = get_the_excerpt($postID);
+        if (strlen($excerpt) < $rules['min-length']) {
+            $errors[] = "Excerpt should be no less than " . $rules['min-length']. " characters in length. " . strlen($excerpt) . " characters detected.";
+        }
+        else if (strlen($excerpt) > $rules['max-length']) {
+            $errors[] = "Excerpt should be no more than " . $rules['max-length']. " characters in length. " . strlen($excerpt) . " characters detected.";
+        }
+
+    }
+    return $errors;
 }
