253fa6130c2cd288f3ae9c48e1d0321ad2e513a0
ChrisBAshton <chrisashtonweb@gmail.com>
2017-06-18 21:35:12 +0100

    small featured_image.php refactor

--- admin/rules/featured_image.php
+++ admin/rules/featured_image.php
@@ -5,31 +5,39 @@ function article_health__featured_image($rules, $postID) {
     if (has_post_thumbnail($postID)) {
         $attachmentID = get_post_thumbnail_id($postID);
         $image = wp_get_attachment_image_src($attachmentID, 'full');
-        $url = $image[0];
-        $width = $image[1];
-        $height = $image[2];
-        $desiredWidth = $rules['dimensions']['width'];
-        $desiredHeight = $rules['dimensions']['height'];
-        $imageType = pathinfo($url, PATHINFO_EXTENSION);
-        $filesizeInKb = getKbSizeOfUrl($url);
-
-        if ($filesizeInKb > $rules['max-size']) {
-            $errors[] = "Max recommended size for Featured Image is " . $rules['max-size'] . "KB but image appears to be " . $filesizeInKb . "KB in size";
-        }
-        if ($imageType !== 'jpg') {
-            $errors[] = "Featured image type should usually be " . $rules['format'] . ", but " . $imageType . " type was detected. Be careful!";
-        }
-        if ($width !== $desiredWidth) {
-            $errors[] = "Featured image width should be " . $desiredWidth . "px, but " . $width . "px was detected";
-        }
-        if ($height !== $desiredHeight) {
-            $errors[] = "Featured image height should be " . $desiredHeight . "px, but " . $height . "px was detected";
-        }
+        checkDimensions($errors, $image, $rules);
+        checkFileInfo($errors, $image, $rules);
     }
     else {
         $errors[] = "No featured image specified!";
     }
     return $errors;
+}
+
+function checkDimensions(&$errors, $image, $rules) {
+    $width = $image[1];
+    $height = $image[2];
+    $desiredWidth = $rules['dimensions']['width'];
+    $desiredHeight = $rules['dimensions']['height'];
+
+    if ($width !== $desiredWidth) {
+        $errors[] = "Featured image width should be " . $desiredWidth . "px, but " . $width . "px was detected";
+    }
+    if ($height !== $desiredHeight) {
+        $errors[] = "Featured image height should be " . $desiredHeight . "px, but " . $height . "px was detected";
+    }
+}
+
+function checkFileInfo(&$errors, $image, $rules) {
+    $url = $image[0];
+    $imageType = pathinfo($url, PATHINFO_EXTENSION);
+    $filesizeInKb = getKbSizeOfUrl($url);
+    if ($filesizeInKb > $rules['max-size']) {
+        $errors[] = "Max recommended size for Featured Image is " . $rules['max-size'] . "KB but image appears to be " . $filesizeInKb . "KB in size";
+    }
+    if ($imageType !== 'jpg') {
+        $errors[] = "Featured image type should usually be " . $rules['format'] . ", but " . $imageType . " type was detected. Be careful!";
+    }
 }
 
 function getKbSizeOfUrl($url) {
