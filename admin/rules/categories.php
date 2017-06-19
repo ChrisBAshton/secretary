d3c7e202c7db43e1c3e9021c166821c8af451809
ChrisBAshton <chrisashtonweb@gmail.com>
2017-06-19 20:31:29 +0100

    implemented category checks

--- admin/rules/categories.php
+++ admin/rules/categories.php
@@ -1,5 +1,36 @@
 <?php
 
 function secretary__categories($rules, $postID) {
-    return ['@TODO secretary__categories'];
+    $errors = [];
+    $categories = get_the_category($postID);
+    $categoryNames = get_category_names_from($categories);
+    checkForForbiddenCategories($errors, $rules, $categoryNames);
+    checkForNonDescriptiveCategories($errors, $rules, $categoryNames);
+    return $errors;
+}
+
+function get_category_names_from($categories) {
+    $mapFunction = function ($category) {
+        return $category->name;
+    };
+    return array_map($mapFunction, $categories);
+}
+
+function checkForForbiddenCategories(&$errors, $rules, $categoryNames) {
+    foreach($rules['not'] as $shouldNotBeThisCategory) :
+        if (in_array($shouldNotBeThisCategory, $categoryNames)) {
+            $errors[] = 'Post is in `' . $shouldNotBeThisCategory . '` category, which is forbidden.';
+        }
+    endforeach;
+}
+
+function checkForNonDescriptiveCategories(&$errors, $rules, $categoryNames) {
+    if (count($categoryNames) > 1) {
+        return;
+    }
+    foreach($rules['not-only'] as $shouldNotBeOnlyThisCategory) :
+        if (in_array($shouldNotBeOnlyThisCategory, $categoryNames)) {
+            $errors[] = 'Post is only in the `' . $shouldNotBeOnlyThisCategory . '` category. It should be in at least one other category.';
+        }
+    endforeach;
 }
